9c1c91054f6682f200863adc89485bc3218ff22e
Harry388 <harrythompson3765@gmail.com>
2023-04-28 13:21:26 +0100

    removed test route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -14,19 +14,6 @@ router.get('/', async (req, res) => {
     res.json(users);
 });
 
-router.get('/test', async (req, res) => {
-    await prisma.message.create({
-        data: {
-            content: 'test',
-            date: new Date(),
-            time: '1',
-            recipientID: 1,
-            senderID: 2
-        }
-    });
-    res.json('done');
-});
-
 router.get('/:id', async (req, res) => {
     const id = Number(req.params.id);
     if (isNaN(id)) {
