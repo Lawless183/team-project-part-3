966d7ba90b5eff6070a24df0df5a7e8a2c940063
KianDoyle <kianrk@icloud.com>
2023-05-08 15:22:54 +0100

    backend test

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -1,23 +1,56 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-const user = 1 // this is for testing purposes 
+// const user = 1 // this is for testing purposes 
 
-async function getMessages() {
-  const messages = await prisma.message.findMany({
-    where: {
-      OR: [
-        { senderID: user },
-        { recipientID: user }
-      ]
-    },
-    select: {
-      dateTime: true,
-      content: true
+// async function getMessages() {
+//   const messages = await prisma.message.findMany({
+//     where: {
+//       OR: [
+//         { senderID: user },
+//         { recipientID: user }
+//       ]
+//     },
+//     select: {
+//       dateTime: true,
+//       content: true
+//     }
+//   });
+//     return messages;
+// }
+
+// console.log(getMessages());
+
+import express from 'express';
+import prisma from '../prisma';
+
+const router = express.Router();
+
+const user = 1;
+
+router.get('/', async (req, res) => {
+  // const messages = await prisma.message.findMany({
+  //   where: {
+  //     OR: [
+  //       { senderID: user },
+  //       { recipientID: user }
+  //     ]
+  //   },
+  //   select: {
+  //     dateTime: true,
+  //     content: true
+  //   }
+  // });
+  // res.json(messages);
+  const users = await prisma.user.findMany({
+    include: {
+        recievedMessages: true,
+        sentMessages: true,
+        groups: true
     }
   });
-    return messages;
-}
+  res.json(users);
+})
 
-console.log(getMessages());
+export default router;
